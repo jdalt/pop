115003f38cf5ad624e47f52c98ab314b1e77df90
Jacob Dalton <jacob.dalton@sportngin.com>
2016-01-14 15:47:41 -0600

    whatever

--- dummy.js
+++ dummy.js
@@ -8,6 +8,12 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.post('/', function (req, res) {
+  console.log('url', req.url)
+  console.log('host', req.headers['host'])
+  res.send('Post World ...');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 })
