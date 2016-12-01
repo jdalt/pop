b0efe37f30b5c79c0e8361626b3072a9ffe72f17
Jacob Dalton <jacobrdalton@gmail.com>
2016-12-01 01:24:07 -0600

    use 20558 for transient port, make pop plist and install files

--- install/install.sh
+++ install/install.sh
@@ -1,5 +1,11 @@
-#!/usr/bin/sh
+#!/usr/bin/env sh
 
-launchctl unload ~/Library/LaunchAgents/cx.pow.powd.plist # take pow dns and http proxy out of the game
-sudo cp dev /etc/resolver/
+# launchctl unload ~/Library/LaunchAgents/cx.pow.powd.plist # take pow dns and http proxy out of the game
+
+cp cx.pop.firewall.plist ~/Library/LaunchAgents/cx.pop.firewall.plist # mac firewall/routing rule
+sudo chown root ~/Library/LaunchAgents/cx.pop.firewall.plist
+sudo chgrp wheel ~/Library/LaunchAgents/cx.pop.firewall.plist
+sudo launchctl load ~/Library/LaunchAgents/cx.pop.firewall.plist
+
+sudo mkdir -p /etc/resolver/  && cp dev /etc/resolver/
 cp ../config/ports_example.json ../config/ports.json
