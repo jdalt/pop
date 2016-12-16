339815a5f91ed0103b984211b9908357268c495d
Scott Olson <scott.olson@sportngin.com>
2016-12-16 16:28:40 -0600

    We are interested in the response body for now, not so much the request body

--- body_proxy.js
+++ body_proxy.js
@@ -1,15 +1,18 @@
 var http = require('http'),
     connect = require('connect'),
-    bodyParser = require('body-parser'),
-    restreamer = require('connect-restreamer'),
+    // bodyParser = require('body-parser'),
+    // restreamer = require('connect-restreamer'),
     proxyKit = require('./lib/proxy-kit.js')()
 
 // TODO: find out why this fails on UserService log ins
 var app = connect()
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({extended: true}))
-  .use(bodyParser.text())
-  .use(restreamer())
+//   .use(bodyParser.json())
+//   .use(bodyParser.urlencoded({extended: true}))
+//   .use(bodyParser.text())
+//   .use(function (req, res) {
+//     console.log('{"scotto":' + JSON.stringify(res.body, null, 2) + '}')
+//   })
+//   .use(restreamer())
   .use(proxyKit.requestHandler)
 
 http.createServer(app).listen(20558)
